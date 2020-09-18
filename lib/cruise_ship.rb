62e30d4a4333cabba8eb490d5fd5be15780a70d1
J K <kj688895@gmail.com>
2020-09-18 21:47:39 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,3 @@
-# passengers = {
-# suite_a: "Amanda Presley",
-# suite_b: "Seymour Hoffman",
-# suite_c: "Alfred Tennyson",
-# suite_d: "Charlie Chaplin",
-# suite_e: "Crumpet the Elf"
-# }
-
 def select_winner(passengers)
   winner = " "
   passengers.each do |suite, name|
@@ -14,4 +6,4 @@ def select_winner(passengers)
     end
   end
 
-  winner 
+  winner
